bab86dc62fbbd0dd4eb9a7fb317631e0477709a4
Maksim Samuilionak <maximwow70@gmail.com>
2017-05-18 22:57:42 +0300

    UI-kits 0.0.8

--- UI-kits/select/select.js
+++ UI-kits/select/select.js
@@ -1,45 +1,47 @@
-function initSelect() {
+function Select(select) {
+    this.ontoggle = function () {
+        select.classList.toggle('select--active');
+    }
+
+    var that = this;
+
+    var input = select.querySelector('.select-input');
+    var btn = select.querySelector('.select-btn');
+
+    var values = select.querySelectorAll('.select-value');
+    for (var j = 0; j < values.length; j++) {
+        var value = values[j].getAttribute('value');
+        values[j].innerHTML = value;
+
+        values[j].innerHTML = value;
+        values[j].addEventListener('click', function (event) {
+            for (var k = 0; k < values.length; k++) {
+                values[k].classList.remove('select-value--active');
+            }
+            this.classList.add('select-value--active');
+
+            input.setAttribute('value', event.currentTarget.value);
+            input.value = event.currentTarget.value;
+            that.ontoggle();
+        });
+    }
+    btn.addEventListener('click', this.ontoggle);
+
+    input.setAttribute('value', values[0].getAttribute('value'));
+}
+
+function initSelects() {
 
     var selects = document.querySelectorAll('.select');
 
     for (var i = 0; i < selects.length; i++) {
-        var select = selects[i];
-        var input = select.querySelector('.select-input');
-        var btn = select.querySelector('.select-btn');
-
-        function toggleSelect() {
-            select.classList.toggle('select--active');
-        }
-
-        btn.addEventListener('click', function () {
-            toggleSelect();
-        });
-
-        var values = select.querySelectorAll('.select-value');
-        for (var j = 0; j < values.length; j++) {
-            var value = values[j].getAttribute('value');
-            values[j].innerHTML = value;
-
-            values[j].innerHTML = value;
-            values[j].addEventListener('click', function (event) {
-                for (var k = 0; k < values.length; k++) {
-                    values[k].classList.remove('select-value--active');
-                }
-                this.classList.add('select-value--active');
-
-                input.setAttribute('value', event.currentTarget.value);
-                input.value = event.currentTarget.value;
-                toggleSelect();
-            });
-        }
-
-        input.setAttribute('value', values[0].getAttribute('value'));
+        var select = new Select(selects[i]);
     }
 
     [].forEach.call(document.querySelectorAll('.select-value_list'), function (el) {
         Ps.initialize(el, { theme: 'square' });
     });
 }
-window.addEventListener('load', function(){
-    initSelect();
+window.addEventListener('load', function () {
+    initSelects();
 });
