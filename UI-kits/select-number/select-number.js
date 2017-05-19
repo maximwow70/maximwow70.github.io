7bc9730d451d20db0a65ba42c07726dc6a5209b7
Maksim Samuilionak <maximwow70@gmail.com>
2017-05-19 13:50:00 +0300

    UI-kits 0.0.9

--- UI-kits/select-number/select-number.js
+++ UI-kits/select-number/select-number.js
@@ -8,9 +8,9 @@ function initSelectNumber() {
         var input = select.querySelector('.select_number-value');
         var value = input.getAttribute('value');
 
-        var min = Number.parseFloat(input.getAttribute('min'));
-        var max = Number.parseFloat(input.getAttribute('max'));
-        var step = Number.parseFloat(input.getAttribute('step'));
+        var min = parseFloat(input.getAttribute('min'));
+        var max = parseFloat(input.getAttribute('max'));
+        var step = parseFloat(input.getAttribute('step'));
 
         function setValue(value) {
             if (value < min) {
@@ -23,8 +23,8 @@ function initSelectNumber() {
         }
 
         input.oninput = function (event) {
-            var value = Number.parseFloat(input.value);
-            if (Number.isNaN(value)) {
+            var value = parseFloat(input.value);
+            if (isNaN(value)) {
                 input.value = 0;
             } else {
                 setValue(value);
@@ -33,12 +33,12 @@ function initSelectNumber() {
 
         var btnIncrement = select.querySelector('.select_number-btn--top');
         btnIncrement.addEventListener('click', function () {
-            setValue(Number.parseFloat(input.value) + step);
+            setValue(parseFloat(input.value) + step);
         });
 
         var btnDecrement = select.querySelector('.select_number-btn--bottom');
         btnDecrement.addEventListener('click', function () {
-            setValue(Number.parseFloat(input.value) - step);
+            setValue(parseFloat(input.value) - step);
         });
     }
 };
