92f4575b4e979d3ac8efb7643cf86a310f984e3b
Maksim Samuilionak <maximwow70@gmail.com>
2017-05-24 16:39:09 +0300

    UI-kits 0.1.3

--- UI-kits/slider/slider.js
+++ UI-kits/slider/slider.js
@@ -30,16 +30,20 @@ function Slider(slider) {
         if (parseFloat(sliderList.style.left) >= 0) {
             btnRight.onclick = function () { };
             btnRight.classList.add('slider-btn--disabled');
+            btnRight.disabled = true;
         } else {
             btnRight.onclick = onBtnRightClick;
             btnRight.classList.remove('slider-btn--disabled');
+            btnRight.disabled = false;
         }
         if (100 - slideWidth * slides.length >= parseFloat(sliderList.style.left)) {
             btnLeft.onclick = function () { };
             btnLeft.classList.add('slider-btn--disabled');
+            btnLeft.disabled = true;
         } else {
             btnLeft.onclick = onBtnLeftClick;
             btnLeft.classList.remove('slider-btn--disabled');
+            btnLeft.disabled = false;
         }
     }
     function onBtnLeftClick() {
@@ -57,6 +61,40 @@ function Slider(slider) {
         manageClick();
     }
 
+
+    var mouseDownX;
+    var mouseMoveX;
+
+    sliderList.onmousedown = function () { };
+    sliderList.onmousemove = function () { };
+    sliderList.onmouseup = function () {};
+
+    function onMouseMove(event) {
+        mouseMoveX = event.screenX;
+        console.log(slideWidth * (slides.length - 1));
+        if (
+            mouseMoveX - mouseDownX > 30 &&
+            -Math.round(parseFloat(sliderList.style.left)) < slideWidth * (slides.length - 1)
+        ) {
+            onBtnLeftClick();
+            sliderList.onmousemove = function () { };
+        }
+        if (
+            mouseDownX - mouseMoveX > 30 &&
+            -Math.round(parseFloat(sliderList.style.left)) > 0
+        ) {
+            onBtnRightClick();
+            sliderList.onmousemove = function () { };
+        }
+    }
+    sliderList.onmousedown = function (event) {
+        mouseDownX = event.screenX;
+        sliderList.onmousemove = onMouseMove;
+    };
+    sliderList.onmouseup = function () {
+        sliderList.onmousemove = function () { };
+    };
+
     window.addEventListener('resize', function () {
         that.init();
     });
@@ -71,5 +109,12 @@ function initSliders() {
         var slider = new Slider(sliders[i]);
     }
 
+    /*[].forEach.call(document.querySelectorAll('.slider-slide_list'), function (el) {
+        Ps.initialize(el);
+
+        document.addEventListener('ps-scroll-x', function(event) {
+            console.log(event);
+        });
+    });*/
 };
 window.addEventListener('load', initSliders);
